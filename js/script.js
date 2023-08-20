5773e0483e74f6217f4be992755fcc99f0e5f1fe
Mahmud256 <mahmud474849@gmail.com>
2023-08-20 18:09:13 +0600

    resolve

--- js/script.js
+++ js/script.js
@@ -1,1 +1,89 @@
-console.log("Mahmud");
+document.getElementById('card-one').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('one-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-two').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('two-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-three').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('three-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-four').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('four-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-five').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('five-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-six').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('six-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-seven').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('seven-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-eight').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('eight-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
+
+document.getElementById('card-nine').addEventListener('click', function () {
+    const newPayAmount = getInputFieldValueById('nine-tk');
+
+    const previousPayTotal = getTextElementValueById('total');
+    const newPayTotal = previousPayTotal + newPayAmount;
+    setTextElementValueById('total', newPayTotal);
+    console.log(newPayTotal);
+    document.getElementById
+})
